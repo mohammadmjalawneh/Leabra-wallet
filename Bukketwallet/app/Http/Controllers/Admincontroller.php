c29750523463d376d2f86a75488c383611cfd177
Your Name <you@example.com>
2021-09-28 20:20:11 +0300

    Code reindent

--- Bukketwallet/app/Http/Controllers/Admincontroller.php
+++ Bukketwallet/app/Http/Controllers/Admincontroller.php
@@ -12,28 +12,28 @@ class Admincontroller extends Controller
     function index()
     {
         if (!empty(session('admin_id'))) {
-            $Adm=new Admin;
-            $users=new Usertable;
-            $result2=$users->all();
-            $id='';
-            $Res=array();
-            $Resf=array();
-            $Tra=new Transactions;
-            foreach ($result2 as $value) {
-                $income=$Tra->where('user_id', '=', $value['user_id'])->
-                where('cat_id', '=', '1')->sum('amount');
-                $outcome=$Tra->where('user_id', '=', $value['user_id'])->
-                where('cat_id', '=', '2')->sum('amount');
-                $Res=array('user_id'=>$value['user_id'],
-                 'outcome'=>$outcome,
-                  'income'=>$income);
-                $Resf=array_merge($Resf, $Res);
+            $adminObject = new Admin;
+            $usersObject = new Usertable;
+            $usersInfo = $usersObject->all();
+            $id = '';
+            $Res = array();
+            $Resf = array();
+            $Tra = new Transactions;
+            foreach ($usersInfo as $value) {
+                $income = $Tra->where('user_id', '=', $value['user_id'])->where('cat_id', '=', '1')->sum('amount');
+                $outcome = $Tra->where('user_id', '=', $value['user_id'])->where('cat_id', '=', '2')->sum('amount');
+                $Res = array(
+                    'user_id' => $value['user_id'],
+                    'outcome' => $outcome,
+                    'income' => $income
+                );
+                $Resf = array_merge($Resf, $Res);
             }
-            $result=$Adm->where('admin_id', '=', session('admin_id'))->get();
+            $result = $adminObject->where('admin_id', '=', session('admin_id'))->get();
             return view('admin/index', [
-                'admin'=>$result,
-                'users'=>$result2,
-                'Sum'=>$Resf,
+                'admin' => $result,
+                'users' => $result2,
+                'Sum' => $Resf,
             ]);
         } else {
             return redirect('Admin/login');
